18e45f49a854f1f1f09075a9be61c4dba96a7674
huxc <455908283@qq.com>
2019-12-31 02:15:38 +0800

    axios

---
+++ src/config/axios.ts
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+const appId = "X15i835PMDNsPcMdod2p9oVk";
+const appSecret = "pF2WqLKSwGQCrHKdzDwZpdov";
+
+const instance = axios.create({
+    baseURL: 'https://gp-server.hunger-valley.com/',
+    headers: {
+        't-app-id': appId,
+        't-app-secret': appSecret,
+    }
+});
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    const xToken = localStorage.getItem('x-token');
+    if (xToken) {
+        config.headers['Authorization'] = `Bearer ${xToken}`
+    }
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    console.log(error);
+    return Promise.reject(error);
+});
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    if (response.headers['x-token']) {
+        localStorage.setItem('x-token', response.headers['x-token'])
+    }
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+
+export default instance
