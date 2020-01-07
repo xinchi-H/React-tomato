0058503770b2160b76e89db360dc9008ebc87db8
huxc <455908283@qq.com>
2020-01-07 17:57:44 +0800

    完成注册页

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import {Input, Icon, Button} from 'antd';
+import {Link} from 'react-router-dom';
 import axios from 'src/config/axios'
+import './SignUp.scss'
 
 interface ISignUpState {
     account: string,
@@ -19,7 +21,7 @@ class SignUp extends React.Component<any, ISignUpState> {
     }
 
     onChangeAccount = (e) => {
-        this.setState({account: e.target.value})
+        this.setState({ account: e.target.value });
     };
     onChangePassword = (e) => {
         this.setState({password: e.target.value})
@@ -44,7 +46,8 @@ class SignUp extends React.Component<any, ISignUpState> {
     render() {
         const {account, password, passwordConformation} = this.state;
         return (
-            <div className='SignUp'>
+            <div className='SignUp' id="SignUp">
+                <h1>番茄闹钟项目注册</h1>
                 <Input
                     placeholder="请输入你的用户名"
                     prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
@@ -61,7 +64,8 @@ class SignUp extends React.Component<any, ISignUpState> {
                     placeholder="请确认密码"
                     onChange={this.onChangePasswordConformation}
                 />
-                <Button onClick={this.submit}>注册</Button>
+                <Button type='primary' className="loginButton" onClick={this.submit}>注册</Button>
+                <p>如果你有账号，请立即<Link to='/login'>登录</Link></p>
             </div>
         );
     }
