3427c556435a1e7ade0a91c9bbb784624730e676
huxc <455908283@qq.com>
2020-01-09 23:38:22 +0800

    完成todoItem

--- src/components/Todos/TodoInput.tsx
+++ src/components/Todos/TodoInput.tsx
@@ -19,13 +19,13 @@ class TodoInput extends React.Component<ITodoInputProps, ITodoInputState> {
 
     onKeyUp = (e) => {
         if (e.keyCode === 13 && this.state.description !== '') {
-            this.addTodo();
-            this.setState({description: ''})
+            this.addTodo()
         }
     };
 
     addTodo = () => {
         this.props.addTodo({description: this.state.description});
+        this.setState({description: ''})
     };
 
     render() {
